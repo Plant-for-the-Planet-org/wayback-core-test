cb841715e8d98f46276b14592ed0ff748eebcae6
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2024-12-05 15:33:51 +0530

    update coordinates to default esri wayback coords
    - also sets up time calculation

--- src/wayback-core-example.js
+++ src/wayback-core-example.js
@@ -1,14 +1,27 @@
 import { XMLHttpRequest } from 'xmlhttprequest';
 global.XMLHttpRequest = XMLHttpRequest;
 
-import { getWaybackItemsWithLocalChanges } from "@vannizhang/wayback-core";
+import { getWaybackItemsWithLocalChanges, getWaybackItems } from "@vannizhang/wayback-core";
 
-const waybackItems = await getWaybackItemsWithLocalChanges(
+const startTime = Date.now();
+const waybackItemsLocal = await getWaybackItemsWithLocalChanges(
 	{
-			longitude: 117.1825,
-			latitude: 34.0556,
+			longitude: -115.2985,
+			latitude:  36.06399999999999
 	},
-	15
+	1
 );
+const endTime = Date.now();
+
+console.log(waybackItemsLocal);
+console.log(`Time to get waybackItems with local changes: ${endTime - startTime}ms`);
+
+/* const startTime2 = Date.now();
+const waybackItems = await getWaybackItems();
+const endTime2 = Date.now();
 
 console.log(waybackItems);
+console.log(`Time to get waybackItems: ${endTime2 - startTime2}ms`); */
+
+
+// lat: 36.06399999999999, lon: -115.2985
